a3a2e07939476af06363c9edd920ed5ebdfe7831
BenedikteEva <cph-bm109@cphbusiness.dk>
2018-10-21 01:45:21 +0200

    tried new way of putting

--- routes/todoapi.js
+++ routes/todoapi.js
@@ -23,7 +23,7 @@ router.get('/', function (req, res, next) {
 
 
   router.put('/todosetchecked/:_id', async function(req,res, next){
-    let todo= await todoFacade.setChecked({_id:req.params._id}, req.params.checked);
+    let todo= await todoFacade.setChecked({_id:req.params._id}, req.body.checked);
    todo.save((err,user, done) => {
       if(err) {
           res.send(err);
