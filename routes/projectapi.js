26ec15ebbc23c2e6331c71c9d742be18e726bfc5
BenedikteEva <cph-bm109@cphbusiness.dk>
2018-10-23 02:17:05 +0200

    corrected project api

--- routes/projectapi.js
+++ routes/projectapi.js
@@ -35,7 +35,7 @@ router.get('/', function (req, res, next) {
      
   });
 
-  router.post('/projectcreate/:project', async function(req,res,next){
+  router.post('/projectcreate', async function(req,res,next){
  
     let newproject =  await projectFacade.addProject(req.body);
   
